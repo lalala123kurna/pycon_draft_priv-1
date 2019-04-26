2e649a249ea448a18e0a04ba462d2d2706015ba5
Dorota Jarecka <djarecka@gmail.com>
2019-04-25 23:33:23 -0400

    fixing 2d (numpy array should have dtype=float, bug in collision 2d); moving to Circle for animations

--- ellastic_collision_1d.py
+++ ellastic_collision_1d.py
@@ -2,6 +2,7 @@ import numpy
 import matplotlib.pyplot as plt
 import matplotlib.animation as anm                                       
 import math as mt
+import pdb
 
 def advection_1d(x_i, v, dt):
     return x_i + v * dt
@@ -13,11 +14,11 @@ def collision_1d(v1_i, v2_i, m1=1, m2=1):
     return v1_f, v2_f
 
 
-def simulation_step(dt, x1_0, x2_0, v1_0, v2_0, domain_x):
+def simulation_step(dt, x1_0, x2_0, v1_0, v2_0, domain_x, radius):
     """ returns positions and velocity  of the particles after one step"""
 
     # radius
-    r1 = r2 = .3
+    r1 = r2 = radius
 
     # positions
     x1 = x1_0
@@ -61,31 +62,34 @@ class Movie:
             - animate(name)    : create the animation; name is the string with file name for the animation
     """
 
-    def __init__(self, dt, t_max, loc, vel, domain_x, ms=30):
+    def __init__(self, dt, t_max, loc, vel, domain_x, radius=1):
 
         self.dt = dt
         self.fps = int(mt.floor(1/dt))
-        self.t_max = t_max
         self.n_max = int(mt.floor(t_max / dt))
         self.loc = loc
         self.vel = vel
         self.domain_x = domain_x
-        self.ms = ms
+        self.radius = radius
 
         self.fig = plt.figure()
         self.fig.subplots_adjust(left=0, bottom=0, right=1, top=1)
 
-        self.ax = self.fig.add_subplot(111, aspect='equal', autoscale_on=False,xlim=(domain_x), ylim=(0,2))
-        self.plot_particles, = self.ax.plot([], [], 'bo')
+        self.ax = self.fig.add_subplot(111, aspect='equal', autoscale_on=False,xlim=(domain_x), ylim=(0,3))
+        self.c1 = plt.Circle((self.loc[0], 1.5), self.radius, color='r', fill=False, linewidth=2)
+        self.c2 = plt.Circle((self.loc[1], 1.5), self.radius, color='r', fill=False, linewidth=2)
+        self.ax.add_patch(self.c1)
+        self.ax.add_patch(self.c2)
 
-        self.frame = plt.Rectangle([0,0], domain_x[1] - domain_x[0], 1, fc='none')
+        self.frame = plt.Rectangle([0,0], domain_x[1] - domain_x[0], 3., fc='none')
         self.ax.add_patch(self.frame)
 
     def generate_frame(self, i):
-        self.loc, self.vel = simulation_step(self.dt, self.loc[0], self.loc[1], self.vel[0], self.vel[1], self.domain_x)
-        self.plot_particles.set_data(self.loc, 1)
-        self.plot_particles.set_markersize(self.ms)
-        return self.plot_particles, self.frame
+        self.loc, self.vel = simulation_step(self.dt, self.loc[0], self.loc[1], self.vel[0], self.vel[1],
+                                             self.domain_x, self.radius)
+        self.c1.set_center((self.loc[0], 1.5))
+        self.c2.set_center((self.loc[1], 1.5))
+        return self.c1, self.c2, self.frame
 
     def animate(self, name):
         animation = anm.FuncAnimation(self.fig, self.generate_frame, frames=self.n_max, interval=10, blit=True)
@@ -93,5 +97,5 @@ class Movie:
         fps = self.fps
         animation.save(name+'.mp4', fps=fps, extra_args=['-vcodec', 'libx264'])
 
-movie = Movie(1/30., 10, [1, 5], [1, -1], [0, 10])
-movie.animate("test2")
+movie = Movie(1/30., 20, [1, 5], [1, -1], [0, 10])
+movie.animate("test_1d")
--- ellastic_collision_2d.py
+++ ellastic_collision_2d.py
@@ -2,6 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as anm                                       
 import math as mt
+import pdb
+
 
 def advection_1d(x_i, v, dt=1):
     return x_i + v * dt
@@ -13,41 +15,45 @@ def collision_1d(v1_i, v2_i, m1=1, m2=1):
     return v1_f, v2_f
 
 
-def collision_2d(v1, v2, dist, m1=1, m2=1):
-    n = dist / np.linalg.norm(dist)
-    # scalars
-    v1n = np.dot(v1, n)
-    v2n = np.dot(v2, n)
-    v1s = v1 - v1n * n
-    v2s = v2 - v2n * n
+def collision_2d(v1, v2, dist, m1, m2):
+    n_vect = dist / np.linalg.norm(dist)
 
-    v1n_f, v2n_f = collision_1d(v1n, v2n, m1, m2) * n
+    # scalars
+    v1n = np.dot(v1, n_vect)
+    v2n = np.dot(v2, n_vect)
+    v1s = v1 - v1n * n_vect
+    v2s = v2 - v2n * n_vect
+
+    v1n_f, v2n_f = collision_1d(v1n, v2n, m1, m2)
+    v1n_f *= n_vect
+    v2n_f *= n_vect
+
     v1_f = v1s + v1n_f
     v2_f = v2s + v2n_f
+    # pdb.set_trace()
     return v1_f, v2_f
 
 
-def simulation_step(dt, loc1_0, loc2_0, v1_0, v2_0, domain):
+def simulation_step(dt, m1, m2, radius, loc1_0, loc2_0, v1_0, v2_0, domain):
     """ returns positions and velocity  of the particles after one step"""
 
     # radius
-    r1 = r2 = 20
+    r1 = r2 = radius
 
     # positions  = (x, y)
     loc1 = loc1_0
     loc2 = loc2_0
     dist = loc2 - loc1
 
-    # elocities
+    # velocities
     v1 = v1_0
     v2 = v2_0
 
-    # do collision between 2 particles
-    for i in [0, 1]:
-        loc1[i] = advection_1d(loc1[i], dt, v1[i])
-        loc2[i] = advection_1d(loc2[i], dt, v2[i])
+
+    # changed the order, previous order didn't work, when the collison
+    # should happen in the first step
     if np.linalg.norm(dist) < (r1 + r2):
-        v1, v2 = collision_2d(v1, v2, dist)
+        v1, v2 = collision_2d(v1, v2, dist, m1, m2)
 
     #boundary condition
     for i in [0, 1]:
@@ -60,43 +66,58 @@ def simulation_step(dt, loc1_0, loc2_0, v1_0, v2_0, domain):
         if loc2[i] + r2 > domain[i][1]:
             v2[i] *= -1
 
+    # do collision between 2 particles
+    for i in [0, 1]:
+        loc1[i] = advection_1d(x_i=loc1[i], v=v1[i], dt=dt)
+        loc2[i] = advection_1d(x_i=loc2[i], v=v2[i], dt=dt)
+
     return [loc1, loc2], [v1, v2]
+
+
+# TODO
+# class Plot_2d:
+#     def __init__(self):
 
 
 class Movie_2d:
     """ TODO """
 
-    def __init__(self, dt, t_max, loc1, loc2, vel1, vel2, domain, ms=30):
-
-        self.dt = dt                                                           
-        self.fps = int(mt.floor(1/dt))                                         
-        self.t_max = t_max                                                     
-        self.n_max = int(mt.floor(t_max / dt))                                 
+    def __init__(self, dt, t_max, loc1, loc2, vel1, vel2, domain, m1=1, m2=1, radius=10):
+        self.dt = dt
+        self.fps = int(mt.floor(1/dt))
+        self.n_max = int(mt.floor(t_max / dt))
         self.loc1 = loc1                                                         
         self.vel1 = vel1
         self.loc2 = loc2                                                         
         self.vel2 = vel2
-        self.domain = domain                                               
-        self.ms = ms
+        self.domain = domain
+        self.m1 = m1
+        self.m2 = m2
+        self.radius = radius
 
         self.fig = plt.figure()                                                
         self.fig.subplots_adjust(left=0, bottom=0, right=1, top=1)             
 
         self.ax = self.fig.add_subplot(111, aspect='equal', autoscale_on=False, xlim=(domain[0]), ylim=domain[1])
-        self.plot_particles, = self.ax.plot([], [], 'bo')                      
+        self.c1 = plt.Circle(self.loc1, self.radius, color='r', fill=False, linewidth=2)
+        self.c2 = plt.Circle(self.loc2, self.radius, color='r', fill=False, linewidth=2)
+        self.ax.add_patch(self.c1)
+        self.ax.add_patch(self.c2)
 
         self.frame = plt.Rectangle([0,0], domain[0][1] - domain[0][0], domain[1][1] - domain[1][0], fc='none')
         self.ax.add_patch(self.frame)        
 
     def generate_frame(self, i):                                               
-        loc_arr, vel_arr = simulation_step(self.dt, self.loc1, self.loc2, self.vel1, self.vel2, self.domain)
+        loc_arr, vel_arr = simulation_step(self.dt, self.m1, self.m2, self.radius,
+                                           self.loc1, self.loc2, self.vel1, self.vel2, self.domain)
         self.loc1 = loc_arr[0]
         self.loc2 = loc_arr[1]
         self.vel1 = vel_arr[0]
         self.vel2 = vel_arr[1]
-        self.plot_particles.set_data([self.loc1[0], self.loc2[0]], [self.loc1[1], self.loc2[1]])                              
-        self.plot_particles.set_markersize(self.ms)                            
-        return self.plot_particles, self.frame                                 
+        self.c1.set_center(self.loc1)
+        self.c2.set_center(self.loc2)
+        # TODO: not sure what should be instead of self.c1 and why
+        return self.c1, self.c2, self.frame
                                                                                
     def animate(self, name):                                                   
         animation = anm.FuncAnimation(self.fig, self.generate_frame, frames=self.n_max, interval=10, blit=True)
@@ -104,11 +125,22 @@ class Movie_2d:
         fps = self.fps                                                         
         animation.save(name+'.mp4', fps=fps, extra_args=['-vcodec', 'libx264'])
 
-loc1 = np.array([30, 50])
-loc2 = np.array([30 + (1200) ** 0.5, 50 + 20])
-v1 = [12, 12]
-v2 = [-8, -15]
-domain = [[0, 100], [0, 100]]
 
-movie = Movie_2d(1/30., 10, loc1, loc2, v1, v2, domain)                             
+# initial values and constants
+loc1_0 = np.array([30., 50.])
+loc2_0 = np.array([30 + (1200) ** 0.5, 50. + 20])
+# loc2_0 = np.array([30 + (1200) ** 0.5, 50.])
+v1 = [12., 12.]
+v2 = [-8, -15.]
+# v1 = [12., 0]
+# v2 = [-8, 0]
+domain = [[0, 100], [0, 100]]
+m1 = 1
+m2 = 2
+dt = 1/30
+t_max = 50
+radius = 10
+
+movie = Movie_2d(dt=dt, t_max=t_max, loc1=loc1_0, loc2=loc2_0,
+                 vel1=v1, vel2=v2, domain=domain, m1=m1, m2=m2, radius=radius)
 movie.animate("test_2d")   
