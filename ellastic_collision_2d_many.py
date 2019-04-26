5a543be2fa8dedc2fdac9fe8bd29b38d8d1f7fc1
Anna Jaruga <ajaruga@caltech.edu>
2019-04-26 06:37:10 -0700

    wip on many collisions

---
+++ ellastic_collision_2d_many.py
@@ -0,0 +1,120 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as anm                                       
+import math as mt
+import pdb
+
+
+def advection_1d(x_i, v, dt=1):
+    return x_i + v * dt
+
+
+def collision_1d(v1_i, v2_i, m1=1, m2=1):
+    v1_f = (m1 - m2) / (m1 + m2) * v1_i + 2 * m2 / (m1 + m2) * v2_i
+    v2_f = (m2 - m1) / (m1 + m2) * v2_i + 2 * m1 / (m1 + m2) * v1_i
+    return v1_f, v2_f
+
+
+def collision_2d(v1, v2, dist, m1, m2):
+    n_vect = dist / np.linalg.norm(dist)
+
+    # scalars
+    v1n = np.dot(v1, n_vect)
+    v2n = np.dot(v2, n_vect)
+    v1s = v1 - v1n * n_vect
+    v2s = v2 - v2n * n_vect
+
+    v1n_f, v2n_f = collision_1d(v1n, v2n, m1, m2)
+    v1n_f *= n_vect
+    v2n_f *= n_vect
+
+    v1_f = v1s + v1n_f
+    v2_f = v2s + v2n_f
+    # pdb.set_trace()
+    return v1_f, v2_f
+
+
+def simulation_step(dt, mass, radius, loc, vel, domain):
+    """ returns positions and velocities of particles after one step"""
+
+    # find pairs of particles undergoing a collision
+    dist = squareform(pdist(loc))
+    ind1, ind2 = np.where(D < 2 * radius)
+    unique = (ind1 < ind2)
+    ind1 = ind1[unique]
+    ind2 = ind2[unique]
+
+    # collisions
+    for id1, id2 in zip(ind1, ind2):
+        vel[id1], vel[id2] = collision_2d(vel[id1], vel[id2], dist[id1,id2], mass[id1], mass[id2])
+
+    # advection
+    loc = advection_1d(x_i=loc, v=vel, dt=dt)
+
+    # find outside domain points ...
+    out_left   = (loc[:, 0] < domain[0][0] + radius)
+    out_right  = (loc[:, 0] > domain[0][1] - radius)
+    out_top    = (loc[:, 1] < domain[1][0] + radius)
+    out_bottom = (loc[:, 1] > domain[1][1] - radius)
+    
+    # TODO ... fix their positions ...?
+    #loc[out_left, 0]   = domain[0][0] + radius
+    #loc[out_right, 0]  = domain[0][1] - radius
+    #loc[out_top, 1]    = domain[1][0] + radius
+    #loc[out_bottom, 1] = domain[1][1] - radius
+
+    # ... change their velocities
+    vel[out_left | out_right,  0] *= -1
+    vel[out_top  | out_bottom, 1] *= -1
+
+    return loc, vel
+
+class Movie_2d:
+    """ TODO """
+
+    def __init__(self, dt, t_max, loc, vel, domain, mass, radius=10):
+        self.dt = dt
+        self.fps = int(mt.floor(1/dt))
+        self.n_max = int(mt.floor(t_max / dt))
+        self.loc = loc                                                         
+        self.vel = vel
+        self.domain = domain
+        self.mass = mass
+        self.radius = radius
+
+        self.fig = plt.figure()                                                
+        self.fig.subplots_adjust(left=0, bottom=0.1, right=1, top=.9)             
+
+        self.ax = self.fig.add_subplot(111, aspect='equal', autoscale_on=False, xlim=(domain[0]), ylim=domain[1])
+        self.circ = plt.Circle(self.loc, self.radius, color='r', fill=False, linewidth=2)
+        self.ax.add_patch(self.circ)
+
+        self.frame = plt.Rectangle([0,0], domain[0][1] - domain[0][0], domain[1][1] - domain[1][0], fc='none')
+        self.ax.add_patch(self.frame)        
+
+    def generate_frame(self, i):                                               
+ 
+        loc, vel = simulation_step(self.dt, self.mass, self.radius,
+                                   self.loc, self.vel, self.domain)
+        self.circ.set_center(self.loc)
+        return self.circ, self.frame
+
+    def animate(self, name):                                                   
+        animation = anm.FuncAnimation(self.fig, self.generate_frame, frames=self.n_max, interval=10, blit=True)
+
+        fps = self.fps                                                         
+        animation.save(name+'.mp4', fps=fps, extra_args=['-vcodec', 'libx264'])
+
+
+# initial values and constants
+loc = np.array([30., 50.] [30 + (1200) ** 0.5, 50. + 20])
+vel = np.array([12., 12.] [-8, -15.])
+domain = [[0, 100] [0, 100]]
+mass = [1, 2]
+dt = 1/30.
+t_max = 1#50
+radius = 10
+ 
+movie = Movie_2d(dt=dt, t_max=t_max, loc=loc,
+                 vel=vel, domain=domain, mass=mass, radius=radius)
+movie.animate("test_2d_many")   
