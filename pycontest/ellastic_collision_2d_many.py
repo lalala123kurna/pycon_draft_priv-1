cd27df822de28848e96b7a55999c0358dc61a300
Anna Jaruga <ajaruga@caltech.edu>
2019-04-29 09:16:51 -0700

    comment out some imports

--- pycontest/ellastic_collision_2d_many.py
+++ pycontest/ellastic_collision_2d_many.py
@@ -1,11 +1,11 @@
 import numpy as np
-import matplotlib
-matplotlib.use("TkAgg")
-import matplotlib.pyplot as plt
-import matplotlib.animation as anm                                       
+#import matplotlib
+#matplotlib.use("TkAgg")
+#import matplotlib.pyplot as plt
+#import matplotlib.animation as anm                                       
 import math as mt
-import pdb
-from scipy.spatial.distance import pdist, squareform
+#import pdb
+#from scipy.spatial.distance import pdist, squareform
 
 from pycontest.collisions import collision_2d
 import pycontest.movies as mv
